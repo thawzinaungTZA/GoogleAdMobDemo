7eff3b2157fb52150f081ff03c55268585daf300
thawzinaung <thawzinaung@aceplussolutions.com>
2018-06-15 10:17:51 +0630

    Initial commit

---
+++ app/src/main/java/hivelocity/co/jp/googleadmobdemo/MainActivity.kt
@@ -0,0 +1,12 @@
+package hivelocity.co.jp.googleadmobdemo
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
